9033be70665587476980ddd55f3ba5d11859f809
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-26 10:09:46 +0300

    initialized filehandler

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/Smook-e/Custom-Relational-Database/filehandler"
 )
 
 
 func main(){
-	fmt.Println("hello")
+	fmt.Println("hello", filehandler.ReadFromFile("databse.txt"), filehandler.WriteToFile("database.txt"))
+
 }
