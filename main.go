32c26b8930a2a13827b9d548abacc8308e9e1789
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-27 16:18:49 +0300

    added code for reading strings from a byte buffer

--- main.go
+++ main.go
@@ -40,23 +40,39 @@ func main(){
 	slice := buffer[page][:]
 
 
-	num1 := binary.BigEndian.Uint32(slice[offset: offset+4])
-
-	offset += 4
-
-	num2 := binary.BigEndian.Uint64(slice[offset:offset+8])
-
-	offset += 8
-
-	fmt.Printf("num1: %d\n num2 : %d", num1,num2)
-	// binary.BigEndian.PutUint32(slice[offset:offset+4], 1234)
-
+	
 	// offset += 4
-	// binary.BigEndian.PutUint64(slice[offset:offset+8], 9999999999)
+	
+	
 	// offset += 8
+	
+	
+	
+	// binary.BigEndian.PutUint32(slice[offset:offset+4], 1234)
+	
+	num1 := binary.BigEndian.Uint32(slice[offset: offset+4])
+	offset += 4
+	// binary.BigEndian.PutUint64(slice[offset:offset+8], 9999999999)
+	num2 := binary.BigEndian.Uint64(slice[offset:offset+8])
+	offset += 8
+	
+	// st := "Hello from database"
+	
+	// st_Len := len(st)
+	
+	// binary.BigEndian.PutUint16(slice[offset:offset + 2], uint16(st_Len))
+	st_len := binary.BigEndian.Uint16(slice[offset: offset+2])
+	offset += 2
+	var st string = string(slice[offset: offset + int(st_len)])
+	offset += int(st_len)
+	
+	// copy(slice[offset: offset + st_Len], st)
+	
+	// offset += st_Len
+	
+	fmt.Printf("num1: %d\nnum2 : %d\nstring: %s", num1,num2, st)
 
-
-	err =  filehandler.WriteToFile(file, page, buffer[page][:])
+	// err =  filehandler.WriteToFile(file, page, buffer[page][:])
 
 	if err != nil {
 		fmt.Println(err) 
