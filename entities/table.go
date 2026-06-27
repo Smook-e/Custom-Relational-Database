22f3fb91713565017c75f9adb6f63b566b4c60f9
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-27 16:45:50 +0300

    added constraints and data types

--- entities/table.go
+++ entities/table.go
@@ -1,6 +1,20 @@
 package entities
 
+// Data type identifiers
+const (
+	TypeSmallInt uint8 = 1 
+	TypeInt     uint8 = 2 
+	TypeBigInt  uint8 = 3 
+	TypeVarChar uint8 = 4 
+)
 
+// Constraint identifiers 
+const (
+	ConstraintNone       uint8 = 0
+	ConstraintPrimaryKey uint8 = 1 << 0 // 1
+	ConstraintNotNull    uint8 = 1 << 1 // 2
+	ConstraintUnique     uint8 = 1 << 2 // 4
+)
 type Table struct {
 
 	rootIndex int
