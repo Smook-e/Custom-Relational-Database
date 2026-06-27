9821af63e1e0129659c4520283846ff54c4245e6
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-27 19:05:10 +0300

    added storage package

---
+++ storage/tables.go
@@ -0,0 +1,14 @@
+package storage
+
+import (
+	// "os"
+	"encoding/binary"
+	"os"
+	"fmt"
+
+	"github.com/Smook-e/Custom-Relational-Database/entities"
+	"github.com/Smook-e/Custom-Relational-Database/filehandler"
+)
+
+
+func CreateTable()
