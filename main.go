d72c941599e9c0f440bdf2dcc404a3582ce0a9f7
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-27 15:47:38 +0300

    added code for reading binary integers from a byte buffer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"encoding/binary"
 	"github.com/Smook-e/Custom-Relational-Database/filehandler"
 )
 
@@ -21,22 +22,47 @@ func main(){
 
 	buffer := make([][4096]byte, 10)
 
-	write := make([]byte, 4096)
+	// write := make([]byte, 4096)
 
-	for i := range write {
-		write[i] = 'A'
-	}
-	page := 2
-
-
-	err =  filehandler.WriteToFile(file, page, write)
+	// for i := range write {
+	// 	write[i] = 'A'
+	// }
+	page := 0
 	
+	err = filehandler.ReadFromFile(file, page, buffer[page][:])
+
 	if err != nil {
-		fmt.Println("an error occured: ", err )
+		fmt.Println(err)
+		return
 	}
+	offset := 0
 
-	err = filehandler.ReadFromFile(file, page, buffer[0][:])
+	slice := buffer[page][:]
 
 
-	fmt.Println(buffer[0][:100])
+	num1 := binary.BigEndian.Uint32(slice[offset: offset+4])
+
+	offset += 4
+
+	num2 := binary.BigEndian.Uint64(slice[offset:offset+8])
+
+	offset += 8
+
+	fmt.Printf("num1: %d\n num2 : %d", num1,num2)
+	// binary.BigEndian.PutUint32(slice[offset:offset+4], 1234)
+
+	// offset += 4
+	// binary.BigEndian.PutUint64(slice[offset:offset+8], 9999999999)
+	// offset += 8
+
+
+	err =  filehandler.WriteToFile(file, page, buffer[page][:])
+
+	if err != nil {
+		fmt.Println(err) 
+		return
+	}
+	
+
+	// fmt.Println(buffer[0][:100])
 }
