ce94006edfef900bf3f410b15608b0de1a46f8f1
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-29 13:05:01 +0300

    added primitive functions for reading and writing freespacepages

---
+++ pages/freeSpacePage.go
@@ -0,0 +1,26 @@
+package pages
+
+import (
+	// "os"
+	// "container/list"
+	"encoding/binary"
+	"fmt"
+	
+	"os"
+
+	"github.com/Smook-e/Custom-Relational-Database/entities"
+	"github.com/Smook-e/Custom-Relational-Database/filehandler"
+	"github.com/Smook-e/Custom-Relational-Database/storage"
+)
+
+
+func ReadFreeSpacePage(db *entities.Database) error {
+
+
+	return nil
+}
+func WriteFreeSpacePage(db *entities.Database) error {
+	
+
+	return nil
+}
