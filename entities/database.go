e0daa2c2bb1f2982df47d7dd4f3538f87517af72
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-29 12:46:56 +0300

    added the freepage struct

--- entities/database.go
+++ entities/database.go
@@ -7,11 +7,14 @@ import (
 	
 )
 
-
+type FreePage struct {
+	PageID	int
+	FreeSpace	int
+}
 type Database struct {
 	File *os.File
 	Tables map[string]*Table
-
+	FreePages	[]FreePage
 	TotalPages int
 
 }
