b9232b5bb4f34792825bc2b89bcc2530515616fb
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-29 13:48:45 +0300

    added ReadFreeSpacePage function

--- pages/freeSpacePage.go
+++ pages/freeSpacePage.go
@@ -15,6 +15,28 @@ import (
 
 
 func ReadFreeSpacePage(db *entities.Database) error {
+	buffer := bufferPool.Get().([]byte)
+	defer bufferPool.Put(buffer)
+	
+	err := filehandler.ReadFromFile(db.File, 1, buffer)
+	if err != nil {
+		return fmt.Errorf("An error occured while reading the Free space pages: %w", err)
+	}
+
+	offset := 0
+	nextPagePointer := binary.BigEndian.Uint16(buffer[offset: offset + 2]); offset += 2;// Read Next Page Id
+
+	numberOfElements := binary.BigEndian.Uint16(buffer[offset: offset + 2]); offset += 2;// Read Number of elements
+
+	freeSpaces := make([]entities.FreePage, 0, numberOfElements)
+	var pageID uint32; var freeSpace uint16;
+
+	for range numberOfElements {
+		pageID = binary.BigEndian.Uint32(buffer[offset:offset + 4]); offset += 4;
+		freeSpace = binary.BigEndian.Uint16(buffer[offset: offset+2]);offset += 2;
+		freeSpaces = append(freeSpaces, entities.FreePage{PageID: pageID, FreeSpace: freeSpace})
+	}
+	db.FreePages = freeSpaces
 
 
 	return nil
@@ -25,7 +47,7 @@ func WriteFreeSpacePage(db *entities.Database) error {
 
 	offset := 0
 	var nextPagePointer uint16 = 0
-	binary.BigEndian.PutUint16(buffer[offset: offset + 2], nextPagePointer); offset += 2 ;
+	binary.BigEndian.PutUint16(buffer[offset: offset + 2], nextPagePointer); offset += 2 ;//Write next page ID
 	binary.BigEndian.PutUint16(buffer[offset: offset + 2] ,uint16(len(db.FreePages))); offset += 2;// number of elements in the page
 	for _, page := range db.FreePages {
 		//PageID
@@ -33,7 +55,8 @@ func WriteFreeSpacePage(db *entities.Database) error {
 		//FreeSpace
 		binary.BigEndian.PutUint16(buffer[offset: offset + 2], page.FreeSpace); offset += 2;
 	}
-	db.File.WriteAt(buffer, 1 * bufferSize)
+	
+	filehandler.WriteToFile(db.File, 1, buffer)
 
 	return nil
 }
