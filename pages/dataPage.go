f91cd925cc3ffe6dd93862585e1e8b6738b4c928
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-30 13:46:03 +0300

    added primitve GetDataPage function

--- pages/dataPage.go
+++ pages/dataPage.go
@@ -5,6 +5,15 @@ import "github.com/Smook-e/Custom-Relational-Database/entities"
 
 func GetDataPage(db *entities.Database, requiredSpace uint16) (uint16, error) {
 	pageID, err:= FindFreePage(db, requiredSpace)
-	
+	if err != nil {
+		return 0, err
+	}
+	// if no Freepage was found
 
+	//Read the Page and determine the slot 
+	buffer := bufferPool.Get().([]byte)
+	defer bufferPool.Put(buffer)
+
+
+	return 0, nil
 }
