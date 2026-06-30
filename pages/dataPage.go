4a4edbe75d2861fd978a4e2c7834ed7f5f0edaa5
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-30 13:57:02 +0300

    added  GetDataPage function

--- pages/dataPage.go
+++ pages/dataPage.go
@@ -1,6 +1,13 @@
 package pages
 
-import "github.com/Smook-e/Custom-Relational-Database/entities"
+import (
+	"github.com/Smook-e/Custom-Relational-Database/entities"
+	"github.com/Smook-e/Custom-Relational-Database/filehandler"
+	"encoding/binary"
+	
+
+
+)
 
 
 func GetDataPage(db *entities.Database, requiredSpace uint16) (uint16, error) {
@@ -14,6 +21,21 @@ func GetDataPage(db *entities.Database, requiredSpace uint16) (uint16, error) {
 	buffer := bufferPool.Get().([]byte)
 	defer bufferPool.Put(buffer)
 
+	err = filehandler.ReadFromFile(db.File, int(pageID), buffer)
+	if err != nil {
+		return 0, err
+	}
+	offset := 0
+	freeSpaceOffset := binary.BigEndian.Uint16(buffer[offset:offset + 2]);
+	freeSpaceOffset -= requiredSpace
+	binary.BigEndian.PutUint16(buffer[offset:offset + 2 ], freeSpaceOffset); offset += 2;
+
+	numberOfElements := binary.BigEndian.Uint16(buffer[offset:offset + 2]);
+	numberOfElements += 1;
+	binary.BigEndian.PutUint16(buffer, numberOfElements)
+	offset += 2 + (int(numberOfElements) * 2)
+	binary.BigEndian.PutUint16(buffer, freeSpaceOffset)
+	
 
 	return 0, nil
 }
