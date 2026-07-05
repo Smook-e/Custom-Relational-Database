61323ed2a8300459e2b1c29323be7dd68aa55c6a
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-05 15:55:48 +0300

    fixed bugs in the test open database function

--- storage/test.go
+++ storage/test.go
@@ -1,39 +1,51 @@
 package storage
 
 
+import (
+	// "errors"
+	
+	"fmt"
+	"os"
 
-func TestOpenDatabase(filename string) error {
-	db, err := OpenDatabase(filename)
+	"github.com/Smook-e/Custom-Relational-Database/entities"
+	
+	"github.com/Smook-e/Custom-Relational-Database/pages"
+)
+
+
+
+func (engine *StorageEngine) TestOpenDatabase(filename string) error {
+	err := engine.TestOpenDatabase(filename)
     if err != nil {
         return fmt.Errorf("OpenDatabase failed: %v", err)
     }
-    defer db.File.Close()
+    defer engine.db.File.Close()
 	// db.File.Truncate(2 * bufferSize)
     
     
-	pageID, slot, err := InsertRow(db, []string{"1", "joe", "20"}, "users")
+	pageID, slot, err := engine.InsertRow([]string{"1", "joe", "20"}, "users")
 	if err != nil {
 		return err
 	}
-	Row, err := ReadRow(db, "users", pageID, slot)
-	if err != nil {
-		return err
-	}
-	fmt.Println(Row)
-	pageID, slot, err = InsertRow(db, []string{"2", "emily", "25"}, "users")
-	if err != nil {
-		return err
-	}
-	Row, err = ReadRow(db, "users", pageID, slot)
+	Row, err := engine.ReadRow( "users", pageID, slot)
 	if err != nil {
 		return err
 	}
 	fmt.Println(Row)
-	pageID, slot, err = InsertRow(db, []string{"1", "Phone", "1000"}, "products")
+	pageID, slot, err = engine.InsertRow( []string{"2", "emily", "25"}, "users")
 	if err != nil {
 		return err
 	}
-	Row, err = ReadRow(db, "products", pageID, slot)
+	Row, err = engine.ReadRow("users", pageID, slot)
+	if err != nil {
+		return err
+	}
+	fmt.Println(Row)
+	pageID, slot, err = engine.InsertRow([]string{"1", "Phone", "1000"}, "products")
+	if err != nil {
+		return err
+	}
+	Row, err = engine.ReadRow("products", pageID, slot)
 	if err != nil {
 		return err
 	}
