07f3b252ba011dc90e1b30c31a639afed8c1e1cc
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-05 15:52:24 +0300

    fixed bugs in the open database function

--- storage/database.go
+++ storage/database.go
@@ -2,34 +2,36 @@ package storage
 
 import (
 	// "errors"
-	"encoding/binary"
-	"fmt"
 	
+	"fmt"
+	"os"
 
 	"github.com/Smook-e/Custom-Relational-Database/entities"
-	"github.com/Smook-e/Custom-Relational-Database/filehandler"
+	
 	"github.com/Smook-e/Custom-Relational-Database/pages"
 )
 
 
-func OpenDatabase(filename string) (*entities.Database, error) {
+func (engine *StorageEngine) OpenDatabase(filename string) ( error) {
 	filep, err :=  os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		return nil, fmt.Errorf("Critical Error: Could not open database file: %w", err)
+		return  fmt.Errorf("Critical Error: Could not open database file: %w", err)
 	}
 	fileInfo, err := filep.Stat()
 	
 	if err != nil {
-		return nil, fmt.Errorf("Failed to retrieve file stats: %w", err)
+		return  fmt.Errorf("Failed to retrieve file stats: %w", err)
 	}
-	db := &entities.Database{
+	engine.db = &entities.Database{
 		File: filep,
 		Tables: make(map[string]*entities.Table),
+		FreePages: make([]entities.FreePage, 0),
 		TotalPages: uint32(fileInfo.Size() / bufferSize),
 	}
-	err = ReadMetaPage(db)
+	
+	err = pages.ReadMetaPage(engine.db)
 	if err != nil {
-		return nil, err
+		return  err
 	}
-	return db, nil
+	return nil
 }
--- storage/storageEngine.go
+++ storage/storageEngine.go
@@ -4,7 +4,7 @@ import (
 	"github.com/Smook-e/Custom-Relational-Database/entities"
 )
 
-
+const bufferSize = 4096
 type StorageEngine struct {
 	db *entities.Database
 
