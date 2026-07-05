01b3f46dab331495fdae4d338a090716747f18cf
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-05 16:40:36 +0300

    added linked list AddNodetoTail

--- utilities/linkedlist/linkedList.go
+++ utilities/linkedlist/linkedList.go
@@ -8,3 +8,21 @@ type Node struct {
 	prev  *Node
 }
 
+func NewNode(value any) *Node {
+	return &Node{Value: value}
+}
+
+func AddNodetoTail(head **Node, value any) {
+	newNode := NewNode(value)
+	if *head == nil {
+		*head = newNode
+		return
+	}
+
+	current := *head
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = newNode
+	newNode.prev = current
+}
