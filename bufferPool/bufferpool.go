6f8b92f6afe9a5f2e7d27413c03d51b3e0a9fbb1
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-06 09:46:11 +0300

    added cache map into BufferPool struct

--- bufferPool/bufferpool.go
+++ bufferPool/bufferpool.go
@@ -1,10 +1,16 @@
 package bufferpool
 
+import (
+	"github.com/Smook-e/Custom-Relational-Database/utilities/linkedlist"
+)
+
 const bufferSize = 4096
 
 
 type BufferPool struct {
 	pages [512]Page
+	cache map[uint32]*linkedlist.Node
+	
 }
 
 type Page struct {
