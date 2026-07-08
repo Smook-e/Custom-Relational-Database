70c39c4cdaeac24695be4a3511240f8ceb1b3ac9
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-08 11:56:31 +0300

    added InternalEntry to indexpage file

--- pages/indexPage.go
+++ pages/indexPage.go
@@ -6,3 +6,9 @@ type LeafEntry struct {
     PageID uint32
     Slot   uint16
 }
+
+type InternalEntry struct {
+    Key     []byte
+    LeftPtr uint32
+	// plus one final RightPtr after all entries
+}
