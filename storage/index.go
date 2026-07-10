dbfc20eed7bc57823b59e2c0fe38b5b6c29ecb3d
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-10 18:45:35 +0300

    added internal node logic in  B+ tree search function

--- storage/index.go
+++ storage/index.go
@@ -3,13 +3,38 @@ package storage
 import (
 	"fmt"
 	"github.com/Smook-e/Custom-Relational-Database/entities"
+	"encoding/binary"
 )
 
-func (engine *StorageEngine) IndexSearch(root uint32, key []byte, typeOfPage uint8) (uint32, uint16, error) {
+func (engine *StorageEngine) IndexSearch(root uint32, key []byte, dataType uint8) (uint32, uint16, error) {
 	buffer , err := engine.Bp.Get(root)
 	if err != nil {
 		return 0, 0, fmt.Errorf("failed to get buffer for page %d: %w", root, err)
 	}
-	
+	// Check if the page is a leaf or internal page
+	offset := 0
+	if buffer[offset] == 0 {
+		// Internal page
+		offset += 1 
+		// Read the number of entries
+		numberOfEntries := binary.BigEndian.Uint16(buffer[offset : offset+2])
+		offset += 2
+		for range numberOfEntries {
+			leftptr := binary.BigEndian.Uint32(buffer[offset : offset+4])
+			offset += 4
+			comp, err := entities.Compare(key, buffer[offset:offset+len(key)], dataType)
+			if err != nil {
+				return 0, 0, fmt.Errorf("comparison error: %w", err)
+			}
+			if comp < 0 {
+				// Key is less than the current entry's key, so we should go to the left pointer
+				return engine.IndexSearch(leftptr, key, dataType)
+			}
+			offset += len(key)
+		}
+		// If we reach here, it means the key is greater than all entries, so we should go to the right pointer
+		rightptr := binary.BigEndian.Uint32(buffer[offset : offset+4])
+		return engine.IndexSearch(rightptr, key, dataType)
+	}
 	return 0, 0, nil
 }
