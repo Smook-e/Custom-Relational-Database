0a98e25fe06ca9f706a102a0cf0e2dd322b8d426
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-10 18:37:39 +0300

    added primitive B+ tree search function

--- storage/index.go
+++ storage/index.go
@@ -2,5 +2,14 @@ package storage
 
 import (
 	"fmt"
+	"github.com/Smook-e/Custom-Relational-Database/entities"
 )
 
+func (engine *StorageEngine) IndexSearch(root uint32, key []byte, typeOfPage uint8) (uint32, uint16, error) {
+	buffer , err := engine.Bp.Get(root)
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to get buffer for page %d: %w", root, err)
+	}
+	
+	return 0, 0, nil
+}
