66be965e83db91b3bcde32860822025e6e6a6c94
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-11 17:22:28 +0300

    added first leaf page in TestIndexPageRoot function

--- storage/testIndex.go
+++ storage/testIndex.go
@@ -7,9 +7,34 @@ import (
 	"github.com/Smook-e/Custom-Relational-Database/pages"
 )
 func (engine *StorageEngine) TestIndexPageRoot() {
-	
+	pageID, err := engine.NewPage()
+	if err != nil {
+		fmt.Println("Error creating new page:", err)
+		return
+	}
+	buffer, err := engine.Bp.Get(pageID)
+	if err != nil {
+		fmt.Println("Error retrieving page from buffer pool:", err)
+		return
+	}
+	// create first leaf page
+	leaf1_keys := []int32{10, 20, 30}
+	leaf1_entries := make([]pages.LeafEntry, len(leaf1_keys))
+	for i, val := range leaf1_keys {
+		key, err := engine.db.Serialize(val, entities.TypeInt)
+		if err != nil {
+			fmt.Println("Error serializing key:", err)
+			return
+		}
+		leaf1_entries[i] = pages.LeafEntry{
+			Key:    key,
+			PageID: uint32(i),
+			Slot:   uint16(i),
+		}
+	}
+	pages.InitializeLeafPage(leaf1_entries, buffer)
+	fmt.Println("Initialized first leaf page with entries:", leaf1_keys)
 }
-
 func (engine *StorageEngine) TestIndexPage() {
 	pageID, err := engine.NewPage()
 	if err != nil {
