7eee8af118c53056390a076fad42c69afcf1bde0
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-19 13:43:13 +0300

    Updated compare function to handle overflow Int

--- entities/table.go
+++ entities/table.go
@@ -259,7 +259,14 @@ func Compare(val1, val2 []byte, dataType uint8) (int, error) {
 	case TypeSmallInt:
 		return int(binary.BigEndian.Uint16(val1)) - int(binary.BigEndian.Uint16(val2)), nil
 	case TypeInt:
-		return int(binary.BigEndian.Uint32(val1)) - int(binary.BigEndian.Uint32(val2)), nil
+		v1 := int32(binary.BigEndian.Uint32(val1))
+		v2 := int32(binary.BigEndian.Uint32(val2))
+		if v1 < v2 {
+			return -1, nil
+		} else if v1 > v2 {
+			return 1, nil
+		}
+		return 0, nil
 	case TypeBigInt:
 		v1 := int64(binary.BigEndian.Uint64(val1))
 		v2 := int64(binary.BigEndian.Uint64(val2))
