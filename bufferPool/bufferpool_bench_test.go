7f1251f21fab801109bcd74a193b5f37f7bcaa7b
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-07-23 17:05:38 +0300

    added ColdRead benchmark for bufferpool bench

--- bufferPool/bufferpool_bench_test.go
+++ bufferPool/bufferpool_bench_test.go
@@ -1,1 +1,14 @@
 package bufferpool
+
+import (
+	"testing"
+)
+
+
+func BenchmarkColdRead(b *testing.B) {
+    // ensure pages aren't cached — e.g. fresh pool each iteration, or evict before each read
+	bp := InitializeBufferPool()
+    for i := 0; i < b.N; i++ {
+        bp.Get(uint32(i  % cacheSize)) // Access pages in a round-robin fashion
+    }
+}
