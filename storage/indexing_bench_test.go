4dd1e8ee68b9647feae957458448fee719c16d33
ahmed <ahmednaser2382005@gmail.com>
2026-07-27 18:11:40 +0300

    testing Linear search

--- storage/indexing_bench_test.go
+++ storage/indexing_bench_test.go
@@ -37,35 +37,35 @@ func BenchmarkIndexSearch(b *testing.B) {
 		}
 	}
 }
-// func BenchmarkLinearSearch(b *testing.B) {
-// 	// Initialize the storage engine and insert 100,000 BigInt keys into the index
-// 	engine, err := InitializeStorageEngine("database.bin")
-// 	if err != nil {
-// 		fmt.Println("Error initializing storage engine:", err)
-// 		return
-// 	}
-// 	defer engine.Bp.File.Close()
-// 	engine.TestWriteandReadDatabase()
-// 	engine, err = InitializeStorageEngine("database.bin")
-// 	if err != nil {
-// 		fmt.Print(err)
-// 		return
-// 	}
+func BenchmarkLinearSearch(b *testing.B) {
+	// Initialize the storage engine and insert 100,000 BigInt keys into the index
+	engine, err := InitializeStorageEngine("database.bin")
+	if err != nil {
+		fmt.Println("Error initializing storage engine:", err)
+		return
+	}
+	defer engine.Bp.File.Close()
+	engine.TestWriteandReadDatabase()
+	engine, err = InitializeStorageEngine("database.bin")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
 
-// 	engine.TestIndexInsertRoot(0)// insert 1000,000 BigInt keys into the index
-// 	var root uint32 = 346 // root page id after inserting 1000,000 BigInt keys
-// 	b.ResetTimer()
-// 	//search 
-// 	for i := 0; i <= 1000000; i++ {
-// 		key, err := engine.db.Serialize(int64(i % 1000000), entities.TypeBigInt)
-// 		if err != nil {
-// 			fmt.Println("Error serializing key:", err)
-// 			return
-// 		}
-// 		_, _, err = engine.LinearSearch(root, key, entities.TypeBigInt)
-// 		if err != nil {
-// 			fmt.Println("Error searching for key:", i, "Error:", err)
-// 			return
-// 		}
-// 	}
-// }
+	engine.TestIndexInsertRoot(0)// insert 1000,000 BigInt keys into the index
+	var root uint32 = 346 // root page id after inserting 1000,000 BigInt keys
+	b.ResetTimer()
+	//search 
+	for i := 0; i <= 1000000; i++ {
+		key, err := engine.db.Serialize(int64(i % 1000000), entities.TypeBigInt)
+		if err != nil {
+			fmt.Println("Error serializing key:", err)
+			return
+		}
+		_, _, err = engine.LinearSearch(root, key, entities.TypeBigInt)
+		if err != nil {
+			fmt.Println("Error searching for key:", i, "Error:", err)
+			return
+		}
+	}
+}
