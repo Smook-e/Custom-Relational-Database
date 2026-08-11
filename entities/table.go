5e588873753667701013a5984cf2081ac5f6c5d4
ahmed <ahmednaser2382005@gmail.com>
2026-08-11 11:03:20 +0300

    added table.go documentation

--- entities/table.go
+++ entities/table.go
@@ -80,6 +80,7 @@ func (col *Column) HasConstraint(constraint uint8) bool {
 
 // GetValues takes a slice of string values and converts them to their respective types based on the column definitions of the table. 
 // It returns a slice containing the converted values, the total size in bytes of the serialized data, a null bitmap indicating which columns are null.
+// it returns an error if any value cannot be converted to the appropriate type.
 func (t *Table) GetValues(vals []string) ([]any,uint16, []byte, error) {
 	values := make([]any, len(vals))
 	var col *Column
