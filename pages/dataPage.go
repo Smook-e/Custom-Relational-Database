e261fc1eea305a723dea795f5bd37aa68b61270a
ahmed <ahmednaser2382005@gmail.com>
2026-08-11 11:25:39 +0300

    added dataPage.go documentation

--- pages/dataPage.go
+++ pages/dataPage.go
@@ -7,12 +7,15 @@ import (
 	"github.com/Smook-e/Custom-Relational-Database/entities"
 	"github.com/Smook-e/Custom-Relational-Database/filehandler"
 )
+/*
+This file contains utility functions for managing data pages in the database.
+It is responsible for reading and writing data pages, managing free space, and handling slots within the pages.
+Used in the storage layer to Insert and Read rows from the database file.
+*/
 
 //Function receives a pageid and slot and reads the page into a buffer and specifies the specific offset of the slot
 func GetDataPageSlotOffset(buffer []byte, slot uint16) (uint16, error) {
-	
 
-	
 	var offset uint16 = 0
 	offset += 2; // free space offset 2 bytes
 	offset += 2 + slot * 2;//each slot has 2 bytes
@@ -21,12 +24,12 @@ func GetDataPageSlotOffset(buffer []byte, slot uint16) (uint16, error) {
 
 }
 
-//Function receives the required space by a row, and returns a buffer, freeSpace offset, numberOfElements(slot) and pageID
+//Function receives the required space by a row and the buffer where the row should be inserted.
+// It updates the free space offset, number of elements, and adds the new slot to the next available position in the data page.
+// it returns the offset where the new row should be inserted and the slot number of the new row.
+// used in the storage layer to insert a new row into a data page.
 func FindandUpdateDataPageSlot( buffer []byte, requiredSpace uint16) (uint16,uint16, error) {
 
-	
-	
-	
 	offset := 0
 	freeSpaceOffset := binary.BigEndian.Uint16(buffer[offset:offset + 2]);
 	freeSpaceOffset -= requiredSpace//update the free space offset
@@ -39,14 +42,14 @@ func FindandUpdateDataPageSlot( buffer []byte, requiredSpace uint16) (uint16,uin
 	
 	return freeSpaceOffset,numberOfElements, nil
 }
-	
+
 func InitializeNewDataPage(db *entities.Database, requiredSpace uint16) error {
 	// fmt.Println("initializing new page")
 	buffer := bufferPool.Get().([]byte)
 	defer bufferPool.Put(buffer)
 	offset := 0
 	binary.BigEndian.PutUint16(buffer[offset: offset + 2], bufferSize - 1); offset += 2;// free space starts from the end of the file since it's still empty
-	binary.BigEndian.PutUint16(buffer[offset:offset + 2], 0)
+	binary.BigEndian.PutUint16(buffer[offset:offset + 2], 0)// number of elements is 0 since it's a new page
 	err := filehandler.WriteToFile(db.File, db.TotalPages, buffer)
 	if err != nil {
 		return err
@@ -54,7 +57,6 @@ func InitializeNewDataPage(db *entities.Database, requiredSpace uint16) error {
 	//add the new free page to the database
 	db.FreePages = append(db.FreePages, entities.FreePage{PageID:db.TotalPages,FreeSpace: bufferSize - 2 - 2 - 2 - requiredSpace})// 2 bytes freeSpaceOffset, 2 numberOfElements, 2 slot, -required space by row
 	
-	// fmt.Println("Added new Data Page", db.TotalPages, "with free space", bufferSize - 2 - 2 - 2 - requiredSpace)	
 	db.TotalPages++
 	return nil
 }
