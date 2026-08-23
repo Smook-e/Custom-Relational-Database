0aad763335d3674213aa885cff73ac643bac194a
ahmed <ahmednaser2382005@gmail.com>
2026-08-23 15:40:07 +0300

    added isDigit func

--- parser/tokenizer.go
+++ parser/tokenizer.go
@@ -27,3 +27,6 @@ var keywords = map[string]bool{
 func isLetter(ch rune) bool {
     return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
 }
+func isDigit(ch rune) bool {
+    return ch >= '0' && ch <= '9'
+}
