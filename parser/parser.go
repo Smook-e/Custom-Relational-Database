f347150eccb22d4df2670bcd574ae73c2fee6fbd
ahmed <ahmednaser2382005@gmail.com>
2026-08-23 18:20:42 +0300

    added helper parser functions

--- parser/parser.go
+++ parser/parser.go
@@ -17,3 +17,22 @@ func NewParser(tokens []Token) *Parser {
 		position: 0,
 	}
 }
+func (p *Parser) Peek() Token {	
+	return p.tokens[p.position]
+}
+
+func (p *Parser) Get() Token {
+	token := p.tokens[p.position]
+	p.position++
+	return token
+}
+func (p *Parser) Expect(expectedType TokenType, val string) (Token, error) {
+	token := p.Peek()
+	if token.Type != expectedType {
+		return token, fmt.Errorf("expected token of type %v, got %v", expectedType, token.Type)
+	}
+	if val != "" && token.Value != val {
+		return token, fmt.Errorf("expected token with value %v, got %v", val, token.Value)
+	}
+	return p.Get(), nil
+}
