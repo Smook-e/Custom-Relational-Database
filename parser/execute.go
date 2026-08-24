4792afc9c618b71b9e1404b37f4f54e41d8506d5
ahmed <ahmednaser2382005@gmail.com>
2026-08-24 13:10:29 +0300

    added Query interface

--- parser/execute.go
+++ parser/execute.go
@@ -1,1 +1,12 @@
 package parser
+
+import (
+	// "fmt"
+	// "strings"
+	"github.com/Smook-e/Custom-Relational-Database/storage"
+)
+
+
+type Query interface {
+	Execute(engine *storage.StorageEngine) (any, error)
+}
