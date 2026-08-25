c5ca9f2afb8cb061e23acd80a8700303e0c70dfd
ahmed <ahmednaser2382005@gmail.com>
2026-08-25 13:01:52 +0300

    Tested B+ Tree on 1,000,000 inserts with middle insertions

--- storage/b+_test.go
+++ storage/b+_test.go
@@ -121,3 +121,64 @@ func TestIndexInsert_ForStrings(t *testing.T) {
 		t.Fatalf("value 1000001 should not be found in index")
 	}
 }
+
+func TestIndexInsert_Middle(t *testing.T) {
+	engine := newEmptyStorageEngine(t)
+
+	// Create a table
+	if err := engine.CreateTable("users", []entities.ColumnDefinition{
+			{Name: "id", DataType: "serial", Constraints: []string{"primarykey"}},
+		}, []entities.ForeignKeyDefinition{}); err != nil {
+			t.Fatalf("failed to create users table: %v", err)
+	}
+	rootID := engine.db.Tables["users"].Indexes["id"]
+	col := engine.db.Tables["users"].Columns[0]
+	// Insert 1,000,000 numbers into the index
+	for i := 0; i <= 1000000; i+=2 {
+		val , err := entities.Serialize(int32(i), &col)
+		if err != nil {
+			t.Fatalf("failed to serialize value: %v", err)
+		}
+		rootID,  err = engine.InsertIntoIndex(rootID, val, uint32(i), uint16(i), &col); 
+		if err != nil {
+			t.Fatalf("failed to insert into users table: %v", err)
+		}
+	}
+	for i := 1; i <= 1000000; i+=2 {
+		val , err := entities.Serialize(int32(i), &col)
+		if err != nil {
+			t.Fatalf("failed to serialize value: %v", err)
+		}
+		rootID,  err = engine.InsertIntoIndex(rootID, val, uint32(i), uint16(i), &col); 
+		if err != nil {
+			t.Fatalf("failed to insert into users table: %v", err)
+		}
+	}
+	// Search for all numbers and verify they exist
+	var pageID uint32
+	for i := 0; i <= 1000000; i++ {
+		val , err := entities.Serialize(int32(i), &col)
+		if err != nil {
+			t.Fatalf("failed to serialize value: %v", err)
+		}
+		pageID, _, err = engine.IndexSearch(rootID, val, &col)
+		if err != nil {
+			t.Fatalf("failed to search in users table: %v", err)
+		}
+		if pageID == 0 {
+			t.Fatalf("value %d not found in index", i)
+		}
+	}
+	// Search for a non-existent number and verify it does not exist
+	val , err := entities.Serialize(int32(1000001), &col)
+	if err != nil {
+		t.Fatalf("failed to serialize value: %v", err)
+	}
+	pageID, _, err = engine.IndexSearch(rootID, val, &col)
+	if err != nil {
+		t.Fatalf("failed to search in users table: %v", err)
+	}
+	if pageID != 0 {
+		t.Fatalf("value 1000001 should not be found in index")
+	}
+}
