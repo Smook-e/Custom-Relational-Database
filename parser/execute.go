3ea4b09711222014210e751f36b02edb36e79cf6
ahmed <ahmednaser2382005@gmail.com>
2026-08-29 11:32:56 +0300

    added DeleteQuery executer

--- parser/execute.go
+++ parser/execute.go
@@ -37,6 +37,9 @@ func (q *SelectQuery) Execute(engine *storage.StorageEngine) (any, error) {
 func (q *InsertQuery) Execute(engine *storage.StorageEngine) (any, error) {
 	return engine.Insert(q.TableName, q.Columns, q.Values)
 }
+func (q *DeleteQuery) Execute(engine *storage.StorageEngine) (any, error) {
+	return engine.Delete(q.TableName, q.Where)
+}
 
 func (Q *QueryHandler) ExecuteQuery(query string) (any, error) {
 	tokens, err := Tokenize(query)
