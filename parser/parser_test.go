6c98974767a190d77583be876555e447bbfb1a64
ahmed <ahmednaser2382005@gmail.com>
2026-09-03 15:17:14 +0300

    updated usage of InitializeQueryHandler

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,19 +4,13 @@ import (
     "path/filepath"
     "testing"
 
-    "github.com/Smook-e/Custom-Relational-Database/storage"
+
 )
 
 func newEmptySQLTestEngine(t *testing.T) *QueryHandler {
     t.Helper()
 
     dbPath := filepath.Join(t.TempDir(), "sql-test.db")
-
-    engine, err := storage.InitializeStorageEngine(dbPath)
-    if err != nil {
-        t.Fatalf("failed to initialize storage engine: %v", err)
-    }
-
     return InitializeQueryHandler(dbPath)
 }
 
