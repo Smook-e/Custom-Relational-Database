054db55e13e52a285162fd702a0eb0bc2446ba03
ahmed <ahmednaser2382005@gmail.com>
2026-09-03 16:26:54 +0300

    added HandleMetaCommand func

--- cli/helpers.go
+++ cli/helpers.go
@@ -1,2 +1,37 @@
 package cli
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Smook-e/Custom-Relational-Database/parser"
+)
+
+func handleMetaCommand(line string, qh *parser.QueryHandler) {
+    switch {
+    case line == "\\t" || line == ".tables":
+        qh.PrintTables()
+
+    case strings.HasPrefix(line, "\\d ") || strings.HasPrefix(line, ".schema "):
+        parts := strings.Fields(line)
+        if len(parts) < 2 {
+            fmt.Println("usage: \\d <table_name>")
+            return
+        }
+        
+		err := qh.PrintTable(parts[1])
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+    case line == "\\?" || line == "help":
+        fmt.Println("\\t          list tables")
+        fmt.Println("\\d <table>  describe table columns")
+        fmt.Println("\\?          show this help")
+        fmt.Println("\\q          quit")
+
+    default:
+        fmt.Println("unknown command:", line)
+    }
+}
